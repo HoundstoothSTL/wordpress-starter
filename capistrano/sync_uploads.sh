cf1ad376376ca95f03c97ef6d4de4b319940b2ce
robbennet <rwbennet@gmail.com>
2013-03-11 15:46:19 -0500

    updated sync_uploads.sh

--- capistrano/sync_uploads.sh
+++ capistrano/sync_uploads.sh
@@ -6,4 +6,4 @@ LOCAL_UPLOADS_DIR=""
 STAGING_UPLOADS_DIR=""
 PRODUCTION_UPLOADS_DIR=""
 
-rsync -avz /Users/rwbennet/Dropbox/Houndstooth/Sites/intelispend/site/wp-content/uploads/ deploy@207.223.251.90:/var/www/domains/intelispend.com/staging/wordpress/shared/uploads/
+# Need to build this out
